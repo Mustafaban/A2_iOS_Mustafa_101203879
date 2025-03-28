89a15b1c7f9a010d303678805d3fe9820cbfe722
Mustafaban <bandukdamustafa1@gmail.com>
2025-03-28 16:55:44 -0400

    added app

---
+++ A2_iOS_Mustafa_101203879/A2_iOS_Mustafa_101203879App.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct A2_iOS_Mustafa_101203879App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+
+
+
