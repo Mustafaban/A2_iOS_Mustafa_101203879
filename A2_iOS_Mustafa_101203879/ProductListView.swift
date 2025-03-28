9008eef1c7d9cf9992bed14c53e3e687c03296dc
Mustafaban <bandukdamustafa1@gmail.com>
2025-03-28 17:45:56 -0400

    Set Up Basic Product List View and ViewModel

--- A2_iOS_Mustafa_101203879/ProductListView.swift
+++ A2_iOS_Mustafa_101203879/ProductListView.swift
@@ -7,4 +7,29 @@ struct ProductListView: View {
     var filteredProducts: [Product] {
         viewModel.products.filter { product in
             searchText.isEmpty ||
-            (
+            (product.name?.localizedCaseInsensitiveContains(searchText) == true) ||
+            (product.desc?.localizedCaseInsensitiveContains(searchText) == true)
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                if filteredProducts.isEmpty {
+                    ContentUnavailableView("No Products", systemImage: "tray", description: Text("Your product list is empty"))
+                } else {
+                    ForEach(filteredProducts, id: \.id) { product in
+                        ProductRowView(product: product)
+                            .listRowSeparator(.hidden)
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .background(Color(.systemGroupedBackground))
+            .navigationTitle("Product Catalog")
+            .onAppear {
+                viewModel.fetchProducts()
+            }
+        }
+    }
+}
